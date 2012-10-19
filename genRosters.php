9d5bc437ae23257daa2df9a5442411481e381c69
Andrew <andrew@lobos.me>
2012-10-19 14:12:14 -0400

    Generate attendance sheets for teachers

---
+++ genRosters.php
@@ -0,0 +1,84 @@
+<?php
+require_once("db.php");
+$careers = $database->getCareers();
+
+?>
+
+<html>
+<head>
+<title>Rosters</title>
+<style type="text/css">
+.schedule
+{
+	page-break-inside:avoid;
+}
+.title
+{
+	text-align: center;
+}
+</style>
+
+</head>
+
+<body>
+<center>
+<?php
+foreach ( $careers as $career )
+{
+	if ( $career['id'] != $assemblyID )
+	{
+		for ( $i = 0; $i < 3; $i++ )
+		{
+			$students = $database->getStudentsIn($career['id'], $i);
+			?>
+			<table class="schedule" border="1">
+			<tr>
+				<td colspan="100%" class="title"><?php echo "<strong>".$career['name']." - Block ".($i+1)."</strong><br>\n"; ?></td>
+			</tr>
+			
+			<tr>
+				<td><strong>ID</strong></td>
+				<td><strong>Name</strong></td>
+			</tr>
+			<?php
+			foreach ($students as $student)
+			{
+				$studentInfo = $database->getStudent($student['id']);
+			?>
+			<tr>
+				<td><?php echo $studentInfo['id']; ?></td>
+				<td><?php echo $studentInfo['first']." ".$studentInfo['last']; ?></td>
+			</tr>
+			<?php
+			}
+			?>
+			</table>
+			<br><br>
+		<?php
+		}
+	}
+
+}
+?>
+
+<table class="schedule" border="1">
+	<tr>
+		<td colspan="100%" class="title"><strong>Sorting Statistics</strong></td>
+	</tr>
+	
+	<?php
+	$stats = $database->getStatistics();
+	foreach ( $stats as $stat )
+	{
+	?>
+	<tr>
+		<td><?php echo $stat['name']; ?></td>
+		<td><?php echo $stat['value']; ?></td>
+	</td>
+	<?php
+	}
+	?>
+</table>
+</center>
+</body>
+</html>
