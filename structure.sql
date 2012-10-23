660121bc0136212fedd6a10b08aa93c29d067c5c
Andrew <andrew@lobos.me>
2012-10-23 13:25:37 -0400

    Added SQL structure dump

---
+++ structure.sql
@@ -0,0 +1,117 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3348
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.1.44)
+# Database: careerday
+# Generation Time: 2012-10-23 17:23:56 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table careers
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `careers`;
+
+CREATE TABLE `careers` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `location` text NOT NULL,
+  `maxStudents` int(11) NOT NULL,
+  `group` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table groups
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `groups`;
+
+CREATE TABLE `groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=100 DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table placements
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `placements`;
+
+CREATE TABLE `placements` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `p1` int(11) NOT NULL,
+  `p2` int(11) NOT NULL,
+  `p3` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=127298 DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table selections
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `selections`;
+
+CREATE TABLE `selections` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `s1` int(11) NOT NULL,
+  `s2` int(11) NOT NULL,
+  `s3` int(11) NOT NULL,
+  `s4` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=127412 DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table statistics
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `statistics`;
+
+CREATE TABLE `statistics` (
+  `name` text NOT NULL,
+  `value` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table students
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `students`;
+
+CREATE TABLE `students` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `first` text NOT NULL,
+  `last` text NOT NULL,
+  `grade` int(11) NOT NULL,
+  `homeroom` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=127412 DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
