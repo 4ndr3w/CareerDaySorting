1776f191d553ffe030afcc6c64a901b4aaeaa0ba
Andrew <andrew@lobos.me>
2012-10-26 14:36:19 -0400

    Began working on the interface for manualy sorting empty slots

---
+++ resolveConflicts.php
@@ -0,0 +1,137 @@
+<?php
+require_once "db.php";
+
+$students = $database->getStudents();
+$careers = $database->getCareers();
+$student = array();
+$placements = array();
+$choices = array();
+foreach ( $students as $_student )
+{
+	$studentIncomplete = false;
+	$_placements = $database->getStudentPlacement($_student['id']);
+	foreach ( $_placements as $p )
+	{
+		if ( $p == 0 )
+		{
+			$studentIncomplete = true;
+			break;
+		}
+	}
+	if ( $studentIncomplete )
+	{
+		$student = $_student;
+		$placements = $_placements;
+		$choices = $database->getStudentChoices($student['id']);
+		break;
+	}
+}
+
+if ( empty($_student) )
+	die("All students have been successfuly sorted");
+
+
+?>
+<html>
+	<head>
+		<title>Manual Sort</title>
+	</head>
+	<body>
+		<center>
+		<table id="info" border="1">
+			<tr>
+				<td colspan="100%"><center><strong>Student Info</strong></center></td>
+			</tr>
+			
+			<tr>
+				<td><strong>ID</strong></td>
+				<td><?php echo $student['id']; ?></td>
+			</tr>
+		
+			<tr>
+				<td><strong>Name</strong></td>
+				<td><?php echo $student['first']." ".$student['last']; ?></td>
+			</tr>
+		
+			<tr>
+				<td><strong>Grade</strong></td>
+				<td><?php echo $student['grade']; ?></td>
+			</tr>
+		</table>
+		<br>
+		
+		<table id="choices" border="1">
+			<tr>
+				<td colspan="100%"><center><strong>Choices</strong></center></td>
+			</tr>
+			<?php 
+			for ($i = 0; $i < 4; $i++ )
+			{
+				$thisChoice = $choices['s'.($i+1)];
+			?>
+				<tr>
+					<td><strong><?php echo $i+1; ?></strong></td>
+					<td><?php echo $careers[$thisChoice]['name']; ?></td>
+				</tr>
+			<?php
+			}
+			?>
+		</table>
+		
+		<br>
+		
+		<table class="schedule" border="1">
+			<tr>
+				<td colspan="100%"><center><strong>Schedule</strong></center></td>
+			</tr>
+			<tr>
+				<td><center><strong>Block</strong></center></td>
+				<td><center><strong>Career</strong></center></td>
+			</tr>
+			<?php
+			for ( $i = 0; $i < 3; $i++ )
+			{
+				$thisBlock = ($i+1);
+			?>
+			<tr>
+				<td><?php echo $thisBlock;?></td>
+				<td>
+					<select>
+					<?php 
+					if ( $placements['p'.$thisBlock] == $assemblyID ) 
+					{ ?>
+						<option selected="selected" id="<?php echo $assemblyID; ?>">Assembly</option>
+					<?php
+					}
+					else
+					{
+						if ( $placements['p'.$thisBlock] == 0 )
+						{
+						?>
+						<option id="0" selected="selected" disabled="disabled">Select One</option> 
+						<?php
+						}
+						
+						foreach ( $careers as $career )
+						{
+							$full = ($database->getNumberOfStudentsInCareer($career['id'], $thisBlock) >= $career['maxStudents']);
+							if ( !$full || $placements['p'.$thisBlock] == $career['id'] )
+							{
+						?>
+							<option id="<?php echo $career['id']; ?>" <?php if ( $career['id'] == $placements['p'.$thisBlock] ) echo "selected=\"selected\""; ?>><?php echo $career['name']; if ( $full ) echo " --FULL--"; ?></option>
+						<?php
+							}
+						}
+					}
+					?>
+					</select>
+				</td>
+			</tr>
+			<?php
+			}
+			?>
+			
+		</table>
+	</center>
+	</body>
+</html>
