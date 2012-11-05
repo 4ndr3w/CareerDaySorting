d53d23c0bdc29f9b673cb133340056c81a87d56b
Andrew <andrew@lobos.me>
2012-11-05 14:31:28 -0500

    Styled signup is the new main signup page

--- signup.php
+++ signup.php
@@ -1,74 +1,237 @@
-<?php
-require_once("db.php");
-
-$db = new Database();
-$notice = "";
-if ( array_key_exists("submit", $_POST) )
-{
-	$result = $db->addStudent($_POST['id'], $_POST['first'], $_POST['last'], $_POST['grade'], $_POST['homeroom']);
-	echo mysql_error();
-	if ( $result )
-	{
-		if ( !$db->setStudentChoices($_POST['id'], $_POST['c1'], $_POST['c2'], $_POST['c3'], $_POST['c4']) )
-			$notice = "Please choose a unique perference for all four choices.";
-		else
-			$notice = "Success!";
-	}
-	else
-	{
-		$notice = "Invalid Student Data";
-	}
-}
-
-$careers = $db->getCareers(false);
-
-
-if ( !empty($notice) )
-{
-	echo $notice."<br><br>";
-	if ( $notice != "Success!")
-	{
-		$database->resetStudent($_POST['id']);
-	}
-}
-?>
-
-<form method="post" action="">
-	Student ID#: <input type="text" name="id"><br>
-	First Name: <input type="text" name="first"><br>
-	Last Name: <input type="text" name="last"><br>
-	Homeroom #: <input type="text" name="homeroom"><br>
-	Grade: <select name="grade">
-		<option value="0" selected="selected" disabled="disabled">Select One</option>
-		<option value="9">Freshman</option>
-		<option value="10">Sophomore</option>
-		<option value="11">Junior</option>
-		<option value="12">Senior</option>
-	</select><br><br>
-	
-	Selections: <br>
-	
-	<?php 
-	for ( $i = 0; $i < 4; $i++ )
-	{
-		$n = $i+1;
-	?>
-		Choice <?php echo $n; ?>: 
-		<select name="c<?php echo $n; ?>">
-			<option value="0" selected="selected" disabled="disabled">Select One</option>
-			<?php 
-			foreach ( $careers as $career )
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Career Day Selection</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="main.css">
+		<script src="jquery.js"></script>
+		<script src="keyValidator.js"></script>
+		<script>
+		var form = 0;
+		
+		function init(){
+			showForm(0);
+		}
+		
+		function showForm(form){
+			for(i=0;i<9;i++)
+				$("#sect-"+i).hide();
+			$("#sect-"+form).fadeIn();
+		}
+		
+		function choicesAreUnique()
+		{
+			for ( a = 5; a <= 8; a++ )
 			{
-			?>
-				<option value="<?php echo $career['id']; ?>"><?php echo $career['name']; ?></option>
-			<?php
+				aVal = document.getElementById("f"+a).value;
+				for ( b = 5; b <= 8; b++ )
+				{
+					bVal = document.getElementById("f"+b).value;
+					if ( a != b && aVal == bVal )
+						return false;
+				}
 			}
-			?>
-		</select><br>
-	<?php
-	}
-	?>
-<br>
+			return true;
+		}
 
-	<input type="submit" name="submit" value="Signup">
-</form>
+		function update(num){
+			if ( num >= 5 && num <= 8 ) // Career lists
+			{
+				var careerList = document.getElementById("f5").options;
+				document.getElementById("c"+num).innerHTML = careerList[document.getElementById("f"+num).value].text;
+			}
+			else // Normal Fields
+				document.getElementById("c"+num).innerHTML = document.getElementById("f"+num).value;
+
+			var process = 0;
+			var bar = 20;
+			var isOptOut = document.getElementById("optOutButton").checked;
+			for(i=0;i<9;i++){
+				if ( isOptOut && i >= 5 && i <= 8 )
+				{
+					process = process + (100/9);
+					bar = bar + (860/9);
+				}
+				else if(document.getElementById("f"+i).value != "" && document.getElementById("f"+i).value != 0){
+					process = process + (100/9);
+					bar = bar + (860/9);
+				}
+			}
+			
+			$("#precent").html(Math.floor(process));
+			$("#InBar").animate({
+				width: bar + "px"},
+				1000,
+				function(){
+					if(bar > 870){
+						if ( choicesAreUnique() )
+							$("#submitarea").slideDown();
+						else
+							$("#submitarea").slideUp();
+					}
+				}
+			);
+		}
+		
+		function seniorCheck(){
+			update(4);
+			if(document.getElementById("f4").value == 12) // Senior Selected
+				document.getElementById("optOutButton").disabled = false;
+			else
+			{
+				document.getElementById("optOutButton").disabled = true;
+				document.getElementById("optInButton").checked = true;
+				document.getElementById("optOutButton").checked = false;
+				disabledChoicesCheck();
+			}
+		}
+		
+		function disabledChoicesCheck()
+		{
+			update(0);
+			state = document.getElementById("optOutButton").checked;
+			var careerList = document.getElementById("f5").options;
+			for ( i = 5; i <= 8; i++ )
+			{
+				document.getElementById("f"+i).disabled = state;
+				
+				if ( state )
+				{
+					document.getElementById("c"+i).innerHTML = "N/A";
+				}
+				else
+				{
+					careerID = document.getElementById("f"+i).value;
+					document.getElementById("c"+i).innerHTML = (careerID==0 ? "" : careerList[careerID].text);
+				}
+			}
+		}
+		
+		function submitToServer()
+		{
+			_id = document.getElementById("f0").value;
+			_first = document.getElementById("f1").value;
+			_last = document.getElementById("f2").value;
+			_homeroom = document.getElementById("f3").value;
+			_grade = document.getElementById("f4").value;
+			dataToSend = "";
+			if ( _grade == 12 && document.getElementById("optOutButton").checked )
+			{
+				dataToSend = {id: _id, first: _first, last: _last, homeroom: _homeroom, grade:_grade, isSeniorOptOut:true};
+			}
+			else
+			{
+				_c1 = document.getElementById("f5").value;
+				_c2 = document.getElementById("f6").value;
+				_c3 = document.getElementById("f7").value;
+				_c4 = document.getElementById("f8").value;
+				dataToSend = {id: _id, first: _first, last: _last, homeroom: _homeroom, grade:_grade, c1: _c1, c2: _c2, c3: _c3, c4: _c4, isSeniorOptOut:false};
+			}
+			
+			$.ajax({
+				type: "POST",
+				url: "signupHandler.php",
+				data: dataToSend,
+				success: function (data)
+				{
+					if ( data == "fail" )
+					{
+						alert("Please make sure that all of the information you have entered is valid.\n");
+					}
+					else
+					{
+						alert("Thank you! Your choices have been recorded.\n");
+						window.location.reload();
+					}
+				}
+			});
+			
+		}
+		</script>
+	</head>
+	<body onload="init()">
+		<div id="container">
+			<section id="header">
+				<h1>Career Day</h1>
+			</section>
+			<div id="content">
+				<section id="process">
+					Process: <span id="precent">0</span>% complete.
+					<div id="ExBar">
+						<div id="InBar"></div>
+					</div>
+					<div id="submitarea">
+						Click here to submit: <button id="submit" type="button" onClick="submitToServer()">Submit</button>
+					</div>
+				</section>
+				<section id="conformation">
+					<a onclick="">Student ID #: <span id="c0"></span><br></a>
+					<a onclick="">First Name: <span id="c1"></span><br></a>
+					<a onclick="">Last Name: <span id="c2"></span><br></a>
+					<a onclick="">Homeroom #: <span id="c3"></span><br></a>						
+					<a onclick="">Grade: <span id="c4"></span><br></a>
+					<a onclick="">Choice 1: <span id="c5"></span><br></a>
+					<a onclick="">Choice 2: <span id="c6"></span><br></a>
+					<a onclick="">Choice 3: <span id="c7"></span><br></a>
+					<a onclick="">Choice 4: <span id="c8"></span><br></a>
+				</section>
+				<section id="formarea">
+					<div id="sect-0" class="sect">
+						Student ID #: <input type="text" id="f0" onblur="update(0)" onkeypress="return validateKeypress(event,2,6)"><br>
+						First Name: <input type="text" id="f1" onblur="update(1)" onkeypress="return validateKeypress(event,1,999)"><br>
+						Last Name: <input type="text" id="f2" onblur="update(2)" onkeypress="return validateKeypress(event,1,999)"><br>
+						Homeroom #: <input type="text" id="f3" onblur="update(3)" onkeypress="return validateKeypress(event,2,3)"><br>
+						Grade:
+						<select id="f4" onchange="seniorCheck()">
+							<option value="0" selected="selected" disabled="disabled">-Select One-</option>
+							<option value="9">Freshman (9)</option>
+							<option value="10">Sophomore (10)</option>
+							<option value="11">Junior (11)</option>
+							<option value="12">Senior (12)</option>
+						</select><br>
+						<button id="next" type="button" onclick="showForm(1)" value="Next">Next</button>
+					</div>
+					<div id="sect-1" class="sect">
+						<input type="radio" id="optOutButton" name="seniorOptOut" value="1" disabled="disabled" onChange="disabledChoicesCheck()">I am going to career shadow or attend a college visit.</input><br><br>
+						<input type="radio" id="optInButton" name="seniorOptOut" value="0" checked="checked" onChange="disabledChoicesCheck()">I am going to participate in career day.</input><br>
+						Choice 1:
+						<select id="f5" onchange="update(5)">
+							<option value="0" selected="selected" disabled="disabled">-Select One-</option>
+							<option value="1">Farmer</option>
+							<option value="2">Sysadmin</option>
+							<option value="3">Programmer</option>
+							<option value="4">Garbage Man</option>
+						</select><br>
+						Choice 2:
+						<select id="f6" onchange="update(6)">
+							<option value="0" selected="selected" disabled="disabled">-Select One-</option>
+							<option value="1">Farmer</option>
+							<option value="2">Sysadmin</option>
+							<option value="3">Programmer</option>
+							<option value="4">Garbage Man</option>
+						</select><br>
+						Choice 3: 
+						<select id="f7" onchange="update(7)">
+							<option value="0" selected="selected" disabled="disabled">-Select One-</option>
+							<option value="1">Farmer</option>
+							<option value="2">Sysadmin</option>
+							<option value="3">Programmer</option>
+							<option value="4">Garbage Man</option>
+						</select><br>
+						Choice 4:
+						<select id="f8" onchange="update(8)">
+							<option value="0" selected="selected" disabled="disabled">-Select One-</option>
+							<option value="1">Farmer</option>
+							<option value="2">Sysadmin</option>
+							<option value="3">Programmer</option>
+							<option value="4">Garbage Man</option>
+						</select><br>
+						<button id="prev" type="button" onclick="showForm(0)" value="Prev">Prev</button>
+					</div>
+					
+				</section>	
+			</div>
+			<section id="footer"></section>
+		</div>
+	</body>
+</html>
---
+++ signupBasic.php
@@ -0,0 +1,74 @@
+<?php
+require_once("db.php");
+
+$db = new Database();
+$notice = "";
+if ( array_key_exists("submit", $_POST) )
+{
+	$result = $db->addStudent($_POST['id'], $_POST['first'], $_POST['last'], $_POST['grade'], $_POST['homeroom']);
+	echo mysql_error();
+	if ( $result )
+	{
+		if ( !$db->setStudentChoices($_POST['id'], $_POST['c1'], $_POST['c2'], $_POST['c3'], $_POST['c4']) )
+			$notice = "Please choose a unique perference for all four choices.";
+		else
+			$notice = "Success!";
+	}
+	else
+	{
+		$notice = "Invalid Student Data";
+	}
+}
+
+$careers = $db->getCareers(false);
+
+
+if ( !empty($notice) )
+{
+	echo $notice."<br><br>";
+	if ( $notice != "Success!")
+	{
+		$database->resetStudent($_POST['id']);
+	}
+}
+?>
+
+<form method="post" action="">
+	Student ID#: <input type="text" name="id"><br>
+	First Name: <input type="text" name="first"><br>
+	Last Name: <input type="text" name="last"><br>
+	Homeroom #: <input type="text" name="homeroom"><br>
+	Grade: <select name="grade">
+		<option value="0" selected="selected" disabled="disabled">Select One</option>
+		<option value="9">Freshman</option>
+		<option value="10">Sophomore</option>
+		<option value="11">Junior</option>
+		<option value="12">Senior</option>
+	</select><br><br>
+	
+	Selections: <br>
+	
+	<?php 
+	for ( $i = 0; $i < 4; $i++ )
+	{
+		$n = $i+1;
+	?>
+		Choice <?php echo $n; ?>: 
+		<select name="c<?php echo $n; ?>">
+			<option value="0" selected="selected" disabled="disabled">Select One</option>
+			<?php 
+			foreach ( $careers as $career )
+			{
+			?>
+				<option value="<?php echo $career['id']; ?>"><?php echo $career['name']; ?></option>
+			<?php
+			}
+			?>
+		</select><br>
+	<?php
+	}
+	?>
+<br>
+
+	<input type="submit" name="submit" value="Signup">
+</form>
