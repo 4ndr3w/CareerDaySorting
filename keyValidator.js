ca28b62d2e03e6cd62d8857a4e95b9e22e25fdab
Andrew <andrew@lobos.me>
2012-11-06 13:39:22 -0500

    Fixed Firefox compatability ( Issue #2 )

--- keyValidator.js
+++ keyValidator.js
@@ -1,21 +1,25 @@
 var lastEvent = undefined;
 function validateKeypress(e,type,length)
 {
+	var target = (e.target ? e.target:e.srcElement);
+	var key = (e.which) ? e.which : ((e.charCode) ? e.charCode : ((e.keyCode) ? e.keyCode : 0));
+	if ( key == 8 || key == 46 || key == 9 ) // Allow backspace, delete, and tab
+		return true;
+
 	switch(type){
 		case 0:
 		case "all":
-			return e.srcElement.value.length < length;
+			return target.value.length < length;
 		case 1:
 		case "text":
-			matchExpression = /[A-z ]/;
+			matchExpression = /[A-z\-]/;
 			break;
 		case 2:
 		case "num":
 			matchExpression = /\d/;
 			break;
 	}
-	if(e.keyCode == 13)
-	e.srcElement.blur();
-	return matchExpression.test(String.fromCharCode(e.keyCode)) && (e.srcElement.value.length < length);
+	if(key == 13)
+		target.blur();
+	return matchExpression.test(String.fromCharCode(key)) && (target.value.length < length);
 }
-
