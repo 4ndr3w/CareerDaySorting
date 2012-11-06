174c7c0239d1f2fdbb5c1428ff1b74fa103b8745
Andrew <andrew@lobos.me>
2012-11-06 14:08:51 -0500

    Pages ready to be loaded via AJAX

--- admin/manageCareers.php
+++ admin/manageCareers.php
@@ -15,48 +15,38 @@ switch ($_POST['action'])
 
 $careers = $database->getCareers();
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Manage Careers</title>
-		<link rel="stylesheet" href="admin.css">
-	</head>
-	
-	<body>
-		<table class="centered" id="info" border="1">
-			<tr>
-				<td><span class="columnHeader">Name</span></td>
-				<td><span class="columnHeader">Location</span></td>
-				<td><span class="columnHeader">Limit</span></td>
-				<td><span class="columnHeader">Actions</span></td>
-			</tr>
-	<?php
-	foreach ( $careers as $career )
-	{
-	?>
+<table class="centered" id="info" border="1">
 		<tr>
-			<form action="" method="post">
-				<input type="hidden" name="action" value="del">
-				<input type="hidden" name="id" value="<?php echo $career['id']; ?>">
-				<td><?php echo $career['name']; ?></td>
-				<td><?php echo $career['location']; ?></td>
-				<td><?php echo $career['maxStudents']; ?></td>
-				<td><input type="submit" value="Delete" class="tableInput"></td>
-			</form>
+			<td><span class="columnHeader">Name</span></td>
+			<td><span class="columnHeader">Location</span></td>
+			<td><span class="columnHeader">Limit</span></td>
+			<td><span class="columnHeader">Actions</span></td>
 		</tr>
-	<?php
-	}
-	?>
-		<tr>
-			<form action="" method="post">
-				<input type="hidden" name="action" value="add">
-				<input type="hidden" name="id" value="<?php echo $career['id']; ?>">
-				<td><input type="text" name="name" class="tableInput"></td>
-				<td><input type="text" name="location" class="tableInput"></td>
-				<td><input type="text" name="maxStudents" class="tableInput"></td>
-				<td><input type="submit" value="Add" class="tableInput"></td>
-			</form>
-		</tr>
-	</table>
-	</body>
-</html>
+<?php
+foreach ( $careers as $career )
+{
+?>
+	<tr>
+		<form action="" method="post">
+			<input type="hidden" name="action" value="del">
+			<input type="hidden" name="id" value="<?php echo $career['id']; ?>">
+			<td><?php echo $career['name']; ?></td>
+			<td><?php echo $career['location']; ?></td>
+			<td><?php echo $career['maxStudents']; ?></td>
+			<td><input type="submit" value="Delete" class="tableInput"></td>
+		</form>
+	</tr>
+<?php
+}
+?>
+	<tr>
+		<form action="" method="post">
+			<input type="hidden" name="action" value="add">
+			<input type="hidden" name="id" value="<?php echo $career['id']; ?>">
+			<td><input type="text" name="name" class="tableInput"></td>
+			<td><input type="text" name="location" class="tableInput"></td>
+			<td><input type="text" name="maxStudents" class="tableInput"></td>
+			<td><input type="submit" value="Add" class="tableInput"></td>
+		</form>
+	</tr>
+</table>
--- admin/tableview.php
+++ admin/tableview.php
@@ -8,39 +8,29 @@ if ( array_key_exists("id", $_POST) )
 $students = $database->getStudents();
 $careers = $database->getCareers();
 ?>
-<html>
-	<head>
-		<title>Table</title>
-	</head>
-	<body>
-		<center>
-		<table id="info" border="1">
-			<tr>
-				<td colspan="100%"><center><strong>Student Info</strong></center></td>
-			</tr>
+<table id="info" border="1">
+	<tr>
+		<td colspan="100%"><center><strong>Student Info</strong></center></td>
+	</tr>
 			
-			<tr>
-				<td><strong>ID</strong></td>
-				<td><strong>Block #1</strong></td>
-				<td><strong>Block #2</strong></td>
-				<td><strong>Block #3</strong></td>
-			</tr>
-			<?php
-			foreach ( $students as $student )
-			{
-				$placements = $database->getStudentPlacement($student['id']);
-			?>
-			<tr>
-				<td><?php echo $student['id']; ?></td>
-				<td><?php echo $careers[$placements['p1']]['name']; ?></td>
-				<td><?php echo $careers[$placements['p2']]['name']; ?></td>
-				<td><?php echo $careers[$placements['p3']]['name']; ?></td>
-			</tr>
-			<?php
-			}
-			?>
-		</table>
-		<br>
-	</center>
-	</body>
-</html>
+	<tr>
+		<td><strong>ID</strong></td>
+		<td><strong>Block #1</strong></td>
+		<td><strong>Block #2</strong></td>
+		<td><strong>Block #3</strong></td>
+	</tr>
+	<?php
+	foreach ( $students as $student )
+	{
+		$placements = $database->getStudentPlacement($student['id']);
+	?>
+	<tr>
+		<td><?php echo $student['id']; ?></td>
+		<td><?php echo $careers[$placements['p1']]['name']; ?></td>
+		<td><?php echo $careers[$placements['p2']]['name']; ?></td>
+		<td><?php echo $careers[$placements['p3']]['name']; ?></td>
+	</tr>
+	<?php
+	}
+	?>
+</table>
