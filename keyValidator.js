8d70c8db72242f1dc86f2b7dc1e6ae0f3915fc4f
Ben <benjamin@thomasnetwork.net>
2012-11-05 22:07:05 -0500

    Fixed keyValidator

--- keyValidator.js
+++ keyValidator.js
@@ -9,7 +9,7 @@ function validateKeypress(e,type,length)
 					return e.srcElement.value.length < length;
 				case 1:
 				case "text":
-					return (e.keyCode > 64 && e.keyCode < 91) || ( e.keyCode > 96 && e.keyCode < 123) || e.keyCode == 45 || e.keyCode == 32);
+					return ((e.keyCode > 64 && e.keyCode < 91) || ( e.keyCode > 96 && e.keyCode < 123) || e.keyCode == 45 || e.keyCode == 32);
 				case 2:
 				case "num":
 					return matchExpression = e.keyCode > 47 && e.keyCode < 58;
