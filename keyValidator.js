d2f5020488bffd7bada0f925fcc7e2273ac7cda9
Ben <benjamin@thomasnetwork.net>
2012-11-05 21:14:00 -0500

    Fixed key issue in Issue #2

--- keyValidator.js
+++ keyValidator.js
@@ -4,7 +4,7 @@ function validateKeypress(e,type,length)
 	switch(type){
 		case 0:
 		case "all":
-			return e.srcElement.value.length < length;
+			return e.target.value.length < length;
 		case 1:
 		case "text":
 			matchExpression = /[A-z ]/;
@@ -15,7 +15,7 @@ function validateKeypress(e,type,length)
 			break;
 	}
 	if(e.keyCode == 13)
-	e.srcElement.blur();
-	return matchExpression.test(String.fromCharCode(e.keyCode)) && (e.srcElement.value.length < length);
+		e.target.blur();
+	return matchExpression.test(String.fromCharCode(e.keyCode)) && (e.target.value.length < length);
 }
 
