99f5744b0574ec6205d73afd413e25091b0997bb
Ben <benjamin@thomasnetwork.net>
2012-11-05 21:51:12 -0500

    Rewrote code handling keypresses in issue #2

--- keyValidator.js
+++ keyValidator.js
@@ -1,21 +1,20 @@
 var lastEvent = undefined;
 function validateKeypress(e,type,length)
 {
-	switch(type){
-		case 0:
-		case "all":
-			return e.target.value.length < length;
-		case 1:
-		case "text":
-			matchExpression = /[A-z ]/;
-			break;
-		case 2:
-		case "num":
-			matchExpression = /\d/;
-			break;
-	}
 	if(e.keyCode == 13)
 		e.target.blur();
-	return matchExpression.test(String.fromCharCode(e.keyCode)) && (e.target.value.length < length);
+			switch(type){
+				case 0:
+				case "all":
+					return e.srcElement.value.length < length;
+				case 1:
+				case "text":
+					return (e.keyCode > 64 && e.keyCode < 91) || ( e.keyCode > 96 && e.keyCode < 123) || e.keyCode == 45 || e.keyCode == 32);
+				case 2:
+				case "num":
+					return matchExpression = e.keyCode > 47 && e.keyCode < 58;
+				break;
+			}
+		return true;
 }
 
