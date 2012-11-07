68cba2082a632fa4d4ab54d67f1f1e3113584788
Andrew <andrew@lobos.me>
2012-11-07 14:41:39 -0500

    <label> on radiobuttons

--- signup.php
+++ signup.php
@@ -194,8 +194,8 @@ $careers = $database->getCareers(false);
 						<button id="next" type="button" onclick="showForm(1)" value="Next">Next</button>
 					</div>
 					<div id="sect-1" class="sect">
-						<input type="radio" id="optOutButton" name="seniorOptOut" value="1" disabled="disabled" onChange="disabledChoicesCheck()">I am going to career shadow or attend a college visit.</input><br><br>
-						<input type="radio" id="optInButton" name="seniorOptOut" value="0" checked="checked" onChange="disabledChoicesCheck()">I am going to participate in career day.</input><br>
+						<input type="radio" id="optOutButton" name="seniorOptOut" value="1" disabled="disabled" onChange="disabledChoicesCheck()" /><label for="optOutButton">I am going to career shadow or attend a college visit.</label><br><br>
+						<input type="radio" id="optInButton" name="seniorOptOut" value="0" checked="checked" onChange="disabledChoicesCheck()" /><label for="optInButton">I am going to participate in career day.</label><br>
 						Choice 1:
 						<select id="f5" onchange="update(5)">
 							<option value="0" selected="selected" disabled="disabled">-Select One-</option>
